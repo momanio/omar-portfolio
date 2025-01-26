1db097a7a3200f1f6e5beae86ac63b0f02c9b850
momanio <o.a.momani@gmail.com>
2025-01-27 01:30:37 +0300

    fixing sanity client

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "cnwn4pjq",
   dataset: import.meta.env.VITE_SANITY_DATASET || "production",
   apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2024-01-01",
   useCdn: false,
